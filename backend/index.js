f8a738d4327af2c5e47c0eced2fad2732573c8ac
ashuyadav998 <137840340+ashuyadav998@users.noreply.github.com>
2025-08-13 18:05:44 +0200

    todo

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import path, { dirname } from 'path';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+
+import productRoutes from './src/routes/productRoutes.js';
+import errorHandler from './src/middlewares/errorHandler.js';
+import connectDB from './src/config/db.js';
+import authRoutes from './src/routes/authRoutes.js';
+import { resolve } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Conectar DB
+connectDB();
+
+// Servir imágenes
+app.use('/images', express.static(resolve(__dirname, 'public')));
+
+// Rutas API
+app.use('/api/products', productRoutes);
+app.use('/api/auth', authRoutes);
+
+// Manejo de errores
+app.use(errorHandler);
+
+// Arrancar servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
