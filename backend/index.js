4d310f5f350f4abe7c1966e9f5a32f0bc02bf923
ashuyadav998 <137840340+ashuyadav998@users.noreply.github.com>
2025-08-14 00:09:30 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,11 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
-app.use(cors({
-  origin: 'https://makhana-shop.netlify.app',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}))
+
 dotenv.config();
 
 const app = express();
@@ -54,3 +50,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use(cors({
+  origin: 'https://makhana-shop.netlify.app',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
