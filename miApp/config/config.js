5894a33848275eec6542c2e83072f14d6de42684
Tech Ashu <ttechashu@gmail.com>
2025-12-15 15:13:19 +0100

    config

--- miApp/config/config.js
+++ miApp/config/config.js
@@ -1,12 +1,12 @@
-// config.js
 import { Platform } from 'react-native';
 
-// ⚠️ IMPORTANTE: Cambia esta IP por la tuya
-// Para obtenerla: ipconfig (Windows) o ifconfig (Mac/Linux)
-const LOCAL_IP = '192.168.1.150'; // ⬅️ CAMBIA ESTO
+// ⚠️ CAMBIA ESTO POR TU URL DE NETLIFY
+const PRODUCTION_API = 'https://https://ashu-shop.vercel.app/';
+
+const LOCAL_IP = '192.168.1.148';
 
 export const API_URL = __DEV__ 
-  ? `http://${LOCAL_IP}:5000/api/auth`  // Desarrollo (dispositivo físico)
-  : 'https://tu-backend-produccion.com/api/auth'; // Producción
+  ? `http://${LOCAL_IP}:5000/api/auth`  // Desarrollo local
+  : `${PRODUCTION_API}/api/auth`;       // Producción
 
 console.log('🚀 Conectando a:', API_URL);
